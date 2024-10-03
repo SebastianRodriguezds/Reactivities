34d666291cfc86ac1a6fcdac1857d72dfa988609
Sebastian <sebastianrodriguezds@gamil.com>
2024-10-03 13:07:54 +0100

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options=>{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.UseAuthentication();
+app.MapControllers();
+
+//destoy as soon as we execute following code.
+using var scope = app.Services.CreateAsyncScope();
+var services = scope.ServiceProvider;
+
+try{
+    var contex = services.GetRequiredService<DataContext>();
+    await contex.Database.MigrateAsync();
+    await Seed.SeedData(contex);
+}
+catch(Exception ex){
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Ah error ocurred during migration");
+}
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
