1c36c01d9b46e88b3812f367e2b6086a405d97d3
Sebastian <sebastianrodriguezds@gamil.com>
2024-10-05 14:57:01 +0100

    CRUD to the API

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,32 @@
-import axios from "axios";
+
 import { useEffect, useState } from "react";
 import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActiviyDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import {v4 as uuid} from 'uuid';
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
 
-
-  useEffect(() => {
-    axios
-      .get<Activity[]>("http://localhost:5000/api/activities")
-
-      .then((response) => {
-        console.log(response);
-        setActivities(response.data);
-      });
-  }, []);
+useEffect(()=>{
+  agent.Activities.list().then(response=>{
+    let activities : Activity[] = [];
+    response.forEach(activity =>{
+      activity.date = activity.date.split('T')[0];
+      activities.push(activity);
+    })
+    setActivities(activities);
+    setLoading(false);
+  })
+}, [])
 
   function handleSelectActivity(id: string){
     setSelectedActivity(activities.find(x => x.id === id));
@@ -41,15 +46,36 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity : Activity) {
-    activity.id ? setActivities([...activities.filter(x=>x.id !== activity.id), activity])
-    : setActivities([...activities, {...activity, id : uuid()}]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if(activity.id){
+      agent.Activities.update(activity).then(()=>{
+        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }else{
+      activity.id = uuid();
+      agent.Activities.create(activity).then(()=>{
+        setActivities([...activities, activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
   }
 
   function handleDeleteActivity(id : string) {
+    setSubmitting(true);
+    agent.Activities.delete(id).then(()=>{
+      setActivities([...activities.filter(x=>x.id !== id)]);
+      setSubmitting(false);
+
+    })
     setActivities([...activities.filter(x=>{return x.id !== id})])
   }
+
+  if (loading) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
@@ -64,6 +90,7 @@ function App() {
         closeForm={handleFormClose}
         createOrEdit = {handleCreateOrEditActivity}
         deleteActivity = {handleDeleteActivity}
+        submitting={submitting}
         ></ActiviyDashboard>
       </Container>
     </>
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { Activity } from "../../../app/models/activity";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 
 interface Props {
   activities: Activity[];
-  selectActivity : (id : string) => void;
-  deleteActivity : (id :string) => void;
+  selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
+export default function ActivityList({
+  activities,
+  selectActivity,
+  deleteActivity,
+  submitting,
+}: Props) {
+  const [target, setTarge] = useState("");
 
-export default function ActivityList({ activities, selectActivity, deleteActivity}: Props) {
+  function handleActivityDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
+    setTarge(e.currentTarget.name);
+    deleteActivity(id);
+  }
+
   return (
     <Segment>
       <Item.Group divided>
@@ -25,9 +40,21 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                 </div>
               </Item.Description>
               <Item.Extra>
-                <Button onClick={()=>selectActivity(activity.id)} floated='right' content='View' color="blue"/>
-                <Button onClick={()=>deleteActivity(activity.id)} floated='right' content='Delete' color="red"/>
-                <Label basic content={activity.category}/>
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated="right"
+                  content="View"
+                  color="blue"
+                />
+                <Button
+                  name={activity.id}
+                  loading={submitting && target===activity.id}
+                  onClick={(e)=> handleActivityDelete(e, activity.id)}
+                  floated="right"
+                  content="Delete"
+                  color="red"
+                />
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
